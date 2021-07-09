5824a04dd72a5f6fe690d7036bd08b7e34274614
何伟峰 <954995880@qq.com>
2021-07-09 10:46:21 +0800

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,30 @@
-import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
-import { eslint } from "rollup-plugin-eslint";
+import babel from 'rollup-plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
+import { eslint } from 'rollup-plugin-eslint'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
     input: 'lib/PasteFromWord.js',
     output: {
         file: 'dist/index.js',
-        name: "PasteFromWord",
-        format: 'umd'
+        name: 'PasteFromWord',
+        format: 'umd',
     },
     plugins: [
         eslint({
             fix: true,
-            exclude: 'node_modules/**'
+            exclude: 'node_modules/**',
         }),
         babel({
-          exclude: 'node_modules/**'
+            exclude: 'node_modules/**',
+            runtimeHelpers: true, // 使plugin-transform-runtime生效
+            externalHelpers: true,
         }),
-        uglify()
-    ]
+        nodeResolve({
+            browser: true,
+        }),
+        commonjs(),
+        uglify(),
+    ],
 }
